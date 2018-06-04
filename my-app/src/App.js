29a52fc6d1ba00e3bc2d0851fa648dd38c9ec3c0
Laura Lucca <laura.lucca@buscapecompany.com>
2018-06-04 17:36:29 -0300

    acessando a api e incluindo autores na tabela

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,131 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
+
+  constructor() {
+    super();    
+    this.state = {lista : [], nome: '', email: '', senha: ''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  componentDidMount(){
+    $.ajax({
+      url:"http://localhost:8080/api/autores",
+      // url:"https://cdc-react.herokuapp.com/api/autores",
+      dataType: 'json',
+      success:function(resposta){    
+        this.setState({lista:resposta});
+      }.bind(this)
+    });       
+  }
+  enviaForm(evento){
+    evento.preventDefault(); 
+    $.ajax({
+      url:"http://localhost:8080/api/autores",
+      // url:"https://cdc-react.herokuapp.com/api/autores",
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+      success: function(resposta) {
+        this.setState({lista:resposta});
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }      
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+  
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }
+  
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="layout">
+
+        <a href="#menu" id="menuLink" className="menu-link">
+
+            <span></span>
+        </a>
+
+        <div id="menu">
+            <div className="pure-menu">
+                <a className="pure-menu-heading" href="#">Company</a>
+
+                <ul className="pure-menu-list">
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+
+
+                </ul>
+            </div>
+        </div>
+
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+          
+            <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post">
+              <div className="pure-control-group">
+                <label htmlFor="nome">Nome</label>
+                <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>  
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" value={this.state.email} onChange={this.setEmail} />           
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="senha">Senha</label>
+                <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />           
+              </div>       
+              <div className="pure-control-group">                                  
+                <label></label> 
+                <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+              </div>
+            </form>             
+
+          </div>  
+          <div>            
+            <table className="pure-table">
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.lista.map(function(autor){
+                    return (
+                      <tr key={autor.id}>
+                        <td>{autor.nome}</td>
+                        <td>{autor.email}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </table> 
+          </div>             
+        </div>
       </div>
     );
   }
